9d18c731c5b39896b52db92cd98451678c9b7be6
Alan Wang <44191076+alankrantas@users.noreply.github.com>
2019-03-28 20:39:45 +0800

    Update test.ts

--- test.ts
+++ test.ts
@@ -1,1 +1,11 @@
-// tests go here; this will not be compiled when this package is used as a library
+DHT11_DHT22.queryData(
+DHTtype.DHT11,
+DigitalPin.P0,
+true,
+true,
+true
+)
+if (DHT11_DHT22.readDataSuccessful()) {
+    basic.showString("T: " + ("" + DHT11_DHT22.readData(dataType.temperature)).substr(0, 2))
+    basic.showString("H: " + ("" + DHT11_DHT22.readData(dataType.humidity)).substr(0, 2))
+}
